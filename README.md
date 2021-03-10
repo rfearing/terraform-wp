c0e0e317f162c48dbe4c2f1d0c6febf768d163fb
Ricardo Fearing <ricardo@heysparkbox.com>
2021-03-10 16:26:19 -0500

    step #2. Get security group attached to ec2 Next Steps: Subnets + RDS security group

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 ### To get started:
 
 1. Set up a `.tfvars` file with `db_username` and `db_password`
-2. Run `terraform init`
-3. Run `terraform apply`
-4. Visit you eC2 instance in AWS.
-5. Ensure there is a security group attached with http incoming traffic allowed. If you get a timeout, there is likely not a security group attached.
+- Run `terraform init`
+- (optionally) run `terraform plan -var-file=".tfvars"` to see changes that will be made
+- Run `terraform apply -var-file=".tfvars"` to apply
+- Visit you eC2 instance in AWS and visit the public IP
--- main.tf
+++ main.tf
@@ -3,15 +3,41 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "wordpress" {
-  ami           = "ami-0015b9ef68c77328d"
-  instance_type = "t2.micro"
-  user_data = file("script.sh")
+resource "aws_security_group" "wordpress_allow_http" {
+  name        = "wordpress_allow_http"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  // IMPORTANT the below should not be used on production:
+  ingress {
+    description = "HTTP Requests"
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "wordpress"
   }
 }
-resource "aws_db_instance" "default" {
+
+resource "aws_instance" "web" {
+  ami           = "ami-0015b9ef68c77328d"
+  instance_type = "t2.micro"
+  user_data = file("script.sh")
+  security_groups = ["${aws_security_group.wordpress_allow_http.name}"]
+  tags = {
+    Name = "wordpress"
+  }
+}
+resource "aws_db_instance" "wpdb" {
   allocated_storage    = 10
   identifier           = "sample"
   storage_type         = "gp2"
@@ -25,3 +51,6 @@ resource "aws_db_instance" "default" {
   skip_final_snapshot  = true
 }
 
+output "IP"  {
+  value = "${aws_instance.web.public_ip}"
+}
